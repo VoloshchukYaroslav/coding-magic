10eef4dac6e571b1917d699fb4501396c5a1b733
nfonnsu <117460447+nfonnsu@users.noreply.github.com>
2025-08-30 02:11:27 +0200

    test

---
+++ src/js/header.js
@@ -0,0 +1,20 @@
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+// Перевірка теми з localStorage
+if (localStorage.getItem('theme') === 'dark') {
+  body.setAttribute('data-theme', 'dark');
+  themeToggle.textContent = '🌙';
+}
+
+themeToggle.addEventListener('click', () => {
+  if (body.getAttribute('data-theme') === 'dark') {
+    body.removeAttribute('data-theme');
+    themeToggle.textContent = '🌞';
+    localStorage.setItem('theme', 'light');
+  } else {
+    body.setAttribute('data-theme', 'dark');
+    themeToggle.textContent = '🌙';
+    localStorage.setItem('theme', 'dark');
+  }
+});
